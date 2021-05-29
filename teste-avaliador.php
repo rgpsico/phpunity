85bb2e4d9dc788769c28202d06ac6a52599c4b47
rgpsico <gerro121@hotmail.com>
2021-05-28 23:26:34 -0300

    aula 02 estrutura de teste

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -19,11 +19,42 @@ $leiloeiro = new Avaliador();
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getmaiorValor();
-if($maiorValor == 2500){
+
+$valorEsperado = 2500;
+if($maiorValor == $valorEsperado){
     echo "teste ok";
 }else{
     echo "teste falhou";
 }
+
+/*
+
+A estrutura de um teste automatizado: 
+Quando falamos de testes automatizados estamos falando de tres partes 
+a primeira parte é  Arrange - Given: 
+Voce preparar o ambiente pro teste  , onde voce cria um cenario , 
+por exemplo : se o usuario no campo nome digitar numeros , qual o resultado teremos ? 
+              se o usuario no campo telefone digitar 20 numeros qual a resposta ele tera 
+              se digitar o padrao correto qual resposta receberar 
+ou seja , a primeira parte onde você vai imaginar varias hipoteses , varios cenários. 
+
+a segunda parte é ACT-When : 
+execultar o codigo que iremos testar  
+
+terceira parte (ASSERT-then):
+e a terceira e saber se o valor esperado e o valor que esperamos.  
+
+padrão:
+arrange act assert 
+ou 
+givewhenthen 
+
+
+
+
+
+
+*/
 
 
 
